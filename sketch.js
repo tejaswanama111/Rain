e0430c9b078d79655b5dd4350a0d62e80a56b294
tejaswanama111 <71207920+tejaswanama111@users.noreply.github.com>
2020-09-16 11:01:25 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var rainSound;
+
+var spot = {
+  x: 100,
+  y: 50
+};
+
+var col = {
+  r: 255,
+  g: 0,
+  b: 0
+};
+
+function preload() {
+  rainSound = loadSound("rain.mp3");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+}
+
+function draw() {
+  
+  rainSound.loop();
+  
+  col.r = random(100, 255);
+  col.g = 0;
+  col.b = random(100, 190);
+
+  spot.x = random(0, width);
+  spot.y = random(0, height);
+  noStroke();
+  fill(col.r, col.g, col.b, 100);
+  ellipse(spot.x, spot.y, 24, 24);
+}
